af92f60c326f40e2a7573d893dc6f24cfb70089d
Dmitriy Kireev <chief_programmer@mail.ru>
2019-02-20 22:42:45 +0300

    Updates

---
+++ precache-manifest.7b40a8317811fce6a8e767e037169530.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e86bdab5a8b2808e963796c334917fb4",
+    "url": "/shop-nyam/static/media/Exo2Thin.e86bdab5.woff2"
+  },
+  {
+    "revision": "5a50f7c24ee67fcb264f5d1cbb4b4892",
+    "url": "/shop-nyam/static/media/Exo2Thin.5a50f7c2.woff"
+  },
+  {
+    "revision": "e04f020e059948e6099e",
+    "url": "/shop-nyam/static/js/runtime~main.e04f020e.js"
+  },
+  {
+    "revision": "e59e12993a9f4e60cf67",
+    "url": "/shop-nyam/static/js/main.e59e1299.chunk.js"
+  },
+  {
+    "revision": "e4ee10d8fbad2046655a",
+    "url": "/shop-nyam/static/js/1.e4ee10d8.chunk.js"
+  },
+  {
+    "revision": "e59e12993a9f4e60cf67",
+    "url": "/shop-nyam/static/css/main.0f209b53.chunk.css"
+  },
+  {
+    "revision": "34087feb86d9ecac96bccfc760ae30bd",
+    "url": "/shop-nyam/index.html"
+  }
+];
